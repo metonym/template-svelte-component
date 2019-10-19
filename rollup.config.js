1fc5d40f409850d84c5d9ddd5f3bc545d7cbf4a9
Eric Liu <ericyl.us@gmail.com>
2019-10-19 10:52:56 -0700

    feat(rollup): add development config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,41 @@
 import typescript from 'rollup-plugin-typescript2';
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import livereload from 'rollup-plugin-livereload';
+import serve from 'rollup-plugin-serve';
+import copy from 'rollup-plugin-copy';
 import pkg from './package.json';
 
 const IS_PROD = !process.env.ROLLUP_WATCH;
 
-export default {
+const configDev = {
+  input: 'src/index.ts',
+  output: {
+    sourcemap: !IS_PROD,
+    format: 'iife',
+    name: 'app',
+    file: 'dist/bundle.js'
+  },
+  plugins: [
+    copy({ targets: [{ src: 'public/*', dest: 'dist' }] }),
+    typescript({ clean: true }),
+    svelte({
+      dev: !IS_PROD,
+      css: css => {
+        css.write('dist/bundle.css', !IS_PROD);
+      }
+    }),
+    resolve(),
+    commonjs(),
+    !IS_PROD && serve({ contentBase: ['dist'], port: 5000 }),
+    !IS_PROD && livereload({ watch: 'dist' }),
+    IS_PROD && terser()
+  ]
+};
+
+const configProd = {
   input: pkg.svelte,
   output: [
     { file: pkg.module, format: 'es' },
@@ -13,3 +43,7 @@ export default {
   ],
   plugins: [typescript(), svelte(), resolve()]
 };
+
+const config = IS_PROD ? configProd : configDev;
+
+export default config;
