30d16fb45ff8f35961c8136f8942580433496737
Eric Liu <ericyl.us@gmail.com>
2020-04-05 13:39:07 -0700

    chore: remove cruft

--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,17 @@
-import typescript from 'rollup-plugin-typescript2';
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import livereload from 'rollup-plugin-livereload';
-import serve from 'rollup-plugin-serve';
-import copy from 'rollup-plugin-copy';
-import pkg from './package.json';
+import resolve from "rollup-plugin-node-resolve";
+import svelte from "rollup-plugin-svelte";
+import pkg from "./package.json";
 
-const IS_PROD = !process.env.ROLLUP_WATCH;
+export default ["es", "umd"].map(format => {
+  const UMD = format === "umd";
 
-const configDev = {
-  input: 'src/index.ts',
-  output: {
-    sourcemap: !IS_PROD,
-    format: 'iife',
-    name: 'app',
-    file: 'dist/bundle.js'
-  },
-  plugins: [
-    copy({ targets: [{ src: 'public/*', dest: 'dist' }] }),
-    typescript({ clean: true }),
-    svelte({
-      dev: !IS_PROD,
-      css: css => {
-        css.write('dist/bundle.css', !IS_PROD);
-      }
-    }),
-    resolve(),
-    commonjs(),
-    !IS_PROD && serve({ contentBase: ['dist'], port: 5000 }),
-    !IS_PROD && livereload({ watch: 'dist' }),
-    IS_PROD && terser()
-  ]
-};
-
-const configProd = {
-  input: pkg.svelte,
-  output: [
-    { file: pkg.module, format: 'es' },
-    { file: pkg.main, format: 'umd', name: 'component' }
-  ],
-  plugins: [typescript(), svelte(), resolve()]
-};
-
-const config = IS_PROD ? configProd : configDev;
-
-export default config;
+  return {
+    input: "src",
+    output: {
+      format,
+      file: UMD ? pkg.main : pkg.module,
+      name: UMD ? "template-svelte-component" : undefined
+    },
+    plugins: [svelte(), resolve()]
+  };
+});
--- src/tests/increment.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('increment', () => {
-  it('increments correctly', () => {
-    expect(increment(0)).toEqual(1);
-  });
-});
-
-function increment(count: number) {
-  return count + 1;
-}
